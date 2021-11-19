c2bef12bdd9670194a44f1b15d5ff6fa8963c42f
dzino <mak-okunev@yandex.ru>
2021-11-19 20:34:55 +0500

    GET / POST / PUT / DELETE

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from rest_framework import routers
 from .views import ManufacturerViewSet, CarViewSet, ManufCarViewSet
 
+# GET / POST / PUT / DELETE
+
 router = routers.SimpleRouter()
 router.register('manufacturer', ManufacturerViewSet, basename='manufacturer') # http://127.0.0.1:8000/api/manufacturer/
-router.register('car', CarViewSet, basename='car') # http://127.0.0.1:8000/api/car/
+router.register('car', CarViewSet, basename='car') # http://127.0.0.1:8000/api/car/ http://127.0.0.1:8000/api/car/1
 router.register('manufacturer_car', ManufCarViewSet, basename='manufacturer_car')
 
 urlpatterns = router.urls
